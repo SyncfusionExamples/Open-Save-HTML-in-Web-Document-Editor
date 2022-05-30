cf1ea00ffb51ff01716cd71cc0f4f20a5b040ff1
Suriya.Murugan <suriya.murugan@syncfusion.com>
2022-05-30 12:42:51 +0530

    Added sample to import and Export HTML

---
+++ ASP.NET Core/DocumenteditorHtml/DocumentEditorController.cs
@@ -0,0 +1,300 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Syncfusion.DocIO;
+using System;
+using System.IO;
+using EJ2DocumentEditor = Syncfusion.EJ2.DocumentEditor;
+using WDocument = Syncfusion.DocIO.DLS.WordDocument;
+using WFormatType = Syncfusion.DocIO.FormatType;
+using Syncfusion.EJ2.DocumentEditor;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace DocumenteditorHtml
+{
+    [Route("api/[controller]")]
+    public class DocumentEditorController : Controller
+    {
+        // GET: api/<controller>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        private IHostingEnvironment hostEnvironment;
+        public DocumentEditorController(IHostingEnvironment environment)
+        {
+            this.hostEnvironment = environment;
+        }
+        //Import file from client side.
+        [Route("Import")]
+        public string Import(IFormCollection data)
+        {
+            if (data.Files.Count == 0)
+                return null;
+            Stream stream = new MemoryStream();
+            IFormFile file = data.Files[0];
+            int index = file.FileName.LastIndexOf('.');
+            string type = index > -1 && index < file.FileName.Length - 1 ?
+                file.FileName.Substring(index) : ".docx";
+            file.CopyTo(stream);
+            stream.Position = 0;
+
+            EJ2DocumentEditor.WordDocument document = EJ2DocumentEditor.WordDocument.Load(stream, GetFormatType(type.ToLower()));
+            string json = Newtonsoft.Json.JsonConvert.SerializeObject(document);
+            document.Dispose();
+            return json;
+        }
+        //Import documents from web server.
+        [Route("ImportFile")]
+        public string ImportFile([FromBody]CustomParams param)
+        {
+            string path = this.hostEnvironment.WebRootPath + "\\Files\\" + param.fileName;
+            try
+            {
+                Stream stream = System.IO.File.Open(path, FileMode.Open, FileAccess.ReadWrite);
+                Syncfusion.EJ2.DocumentEditor.WordDocument document = Syncfusion.EJ2.DocumentEditor.WordDocument.Load(stream, GetFormatType(path));
+                string json = Newtonsoft.Json.JsonConvert.SerializeObject(document);
+                document.Dispose();
+                stream.Dispose();
+                return json;
+            }
+            catch
+            {
+                return "Failure";
+            }
+        }
+        [Route("SystemClipboard")]
+        public string SystemClipboard([FromBody]CustomParameter param)
+        {
+            if (param.content != null && param.content != "")
+            {
+                try
+                {
+                    WordDocument document = WordDocument.LoadString(param.content, GetFormatType(param.type.ToLower()));
+                    string json = Newtonsoft.Json.JsonConvert.SerializeObject(document);
+                    document.Dispose();
+                    return json;
+                }
+                catch (Exception)
+                {
+                    return "";
+                }
+            }
+            return "";
+        }
+
+        public class SaveParam
+        {
+            public string content { get; set; }
+        }
+
+        public class CustomParams
+        {
+            public string fileName { get; set; }
+        }
+
+        public class SaveParameter
+        {
+            public string Content { get; set; }
+            public string FileName { get; set; }
+        }
+        [Route("Save")]
+        public void Save([FromBody] SaveParameter data)
+        {
+            string name = data.FileName;
+            string format = RetrieveFileType(name);
+            if (string.IsNullOrEmpty(name))
+            {
+                name = "Document1.doc";
+            }
+            WDocument document = WordDocument.Save(data.Content);
+            FileStream fileStream = new FileStream(name, FileMode.OpenOrCreate, FileAccess.ReadWrite);
+            document.Save(fileStream, GetWFormatType(format));
+            document.Close();
+            fileStream.Close();
+        }
+
+
+        [Route("ExportSFDT")]
+        public FileStreamResult ExportSFDT([FromBody] SaveParameter data)
+        {
+            string name = data.FileName;
+            string format = RetrieveFileType(name);
+            if (string.IsNullOrEmpty(name))
+            {
+                name = "Document1.doc";
+            }
+            WDocument document = WordDocument.Save(data.Content);
+            return SaveDocument(document, format, name);
+        }
+
+        private string RetrieveFileType(string name)
+        {
+            int index = name.LastIndexOf('.');
+            string format = index > -1 && index < name.Length - 1 ?
+                name.Substring(index) : ".doc";
+            return format;
+        }
+
+
+        [Route("Export")]
+        public FileStreamResult Export(IFormCollection data)
+        {
+            if (data.Files.Count == 0)
+                return null;
+            string fileName = this.GetValue(data, "filename");
+            string name = fileName;
+            string format = RetrieveFileType(name);
+            if (string.IsNullOrEmpty(name))
+            {
+                name = "Document1";
+            }
+            WDocument document = this.GetDocument(data);
+            return SaveDocument(document, format, fileName);
+        }
+
+        private FileStreamResult SaveDocument(WDocument document, string format, string fileName)
+        {
+            Stream stream = new MemoryStream();
+            string contentType = "";
+            if (format == ".pdf")
+            {
+                contentType = "application/pdf";
+            }
+            else
+            {
+                WFormatType type = GetWFormatType(format);
+                switch (type)
+                {
+                    case WFormatType.Rtf:
+                        contentType = "application/rtf";
+                        break;
+                    case WFormatType.WordML:
+                        contentType = "application/xml";
+                        break;
+                    case WFormatType.Html:
+                        contentType = "application/html";
+                        break;
+                    case WFormatType.Dotx:
+                        contentType = "application/vnd.openxmlformats-officedocument.wordprocessingml.template";
+                        break;
+                    case WFormatType.Doc:
+                        contentType = "application/msword";
+                        break;
+                    case WFormatType.Dot:
+                        contentType = "application/msword";
+                        break;
+                }
+                document.Save(stream, type);
+            }
+            document.Close();
+            stream.Position = 0;
+            return new FileStreamResult(stream, contentType)
+            {
+                FileDownloadName = fileName
+            };
+        }
+        internal static WFormatType GetWFormatType(string format)
+        {
+            if (string.IsNullOrEmpty(format))
+                throw new NotSupportedException("EJ2 DocumentEditor does not support this file format.");
+            switch (format.ToLower())
+            {
+                case ".dotx":
+                    return WFormatType.Dotx;
+                case ".docx":
+                    return WFormatType.Docx;
+                case ".docm":
+                    return WFormatType.Docm;
+                case ".dotm":
+                    return WFormatType.Dotm;
+                case ".dot":
+                    return WFormatType.Dot;
+                case ".doc":
+                    return WFormatType.Doc;
+                case ".rtf":
+                    return WFormatType.Rtf;
+                case ".html":
+                    return WFormatType.Html;
+                case ".txt":
+                    return WFormatType.Txt;
+                case ".xml":
+                    return WFormatType.WordML;
+                case ".odt":
+                    return WFormatType.Odt;
+                default:
+                    throw new NotSupportedException("EJ2 DocumentEditor does not support this file format.");
+            }
+        }
+        internal static EJ2DocumentEditor.FormatType GetFormatType(string fileName)
+        {
+            int index = fileName.LastIndexOf('.');
+            string format = index > -1 && index < fileName.Length - 1 ? fileName.Substring(index + 1) : "";
+
+            if (string.IsNullOrEmpty(format))
+                throw new NotSupportedException("EJ2 Document editor does not support this file format.");
+            switch (format.ToLower())
+            {
+                case "dotx":
+                case "docx":
+                case "docm":
+                case "dotm":
+                    return EJ2DocumentEditor.FormatType.Docx;
+                case "dot":
+                case "doc":
+                    return EJ2DocumentEditor.FormatType.Doc;
+                case "rtf":
+                    return EJ2DocumentEditor.FormatType.Rtf;
+                case "html":
+                    return EJ2DocumentEditor.FormatType.Html;
+                case "txt":
+                    return EJ2DocumentEditor.FormatType.Txt;
+                case "xml":
+                    return EJ2DocumentEditor.FormatType.WordML;
+                default:
+                    throw new NotSupportedException("EJ2 Document editor does not support this file format.");
+            }
+        }
+        public class ExportData
+        {
+            public string fileName { get; set; }
+            public string documentData { get; set; }
+        }
+
+        public class CustomParameter
+        {
+            public string content { get; set; }
+            public string type { get; set; }
+        }
+        private string GetValue(IFormCollection data, string key)
+        {
+            if (data.ContainsKey(key))
+            {
+                string[] values = data[key];
+                if (values.Length > 0)
+                {
+                    return values[0];
+                }
+            }
+            return "";
+        }
+        private WDocument GetDocument(IFormCollection data)
+        {
+            Stream stream = new MemoryStream();
+            IFormFile file = data.Files[0];
+            file.CopyTo(stream);
+            stream.Position = 0;
+
+            WDocument document = new WDocument(stream, WFormatType.Docx);
+            stream.Dispose();
+            return document;
+        }
+    }
+}
